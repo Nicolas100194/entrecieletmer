63622a17bf493635d37bac64a2bce0f47792b287
Nicolas <nicolasdelacroix10@gmail.com>
2022-10-25 11:25:47 +0200

    intégration page accueil

--- front-page.php
+++ front-page.php
@@ -1,3 +1,99 @@
 <?php get_header();?>
 <?php the_content();?>
+    <section class="container-presentation">
+        <div class="container-img-presentation">
+            <img class="trace-presentation" src="wp-content/themes/entrecieletmer/assets/img/trace_portrait.png">
+            <img class="img-presentation" src="wp-content/themes/entrecieletmer/assets/img/portrait.png">
+        </div>
+        <div class="container-content-presentation">
+            <div class="container-txt-presentation">
+                <h2 class="title-presentation">Un lieu dédié au bien être des femmes</h2>
+                <p>Je suis Adeline, infirmière diplômée en Hypnose thérapeutique et praticienne
+                    périnatale à Troyes.
+                    Spécialisée en suivi de Parcours PMA et FIV, Grossesse et Périnatalité, Ménopause,
+                    Gestion du stress et des émotions, ma vocation est
+                    avant tout d’accompagner les femmes pour les aider à s’épanouir à chaque étape de
+                    leur vie, de la puberté à la maternité sans oublier la ménopause.
+
+                    La vie d’une femme est faite d’une multitude de changements qui nécessitent remise
+                    en questions et adaptations constante.
+                    Il est parfois facile de se laisser submerger.
+
+                    J’ai fait le choix d’intervenir dans les étapes clefs de la vie d’une femme,
+                    afin que vous puissiez vivre ces profonds changements avec sérénité.</p>
+                <button>me découvrir</button>
+            </div>
+        </div>
+    </section>
+    <section class="container-cabinet">
+        <img class="trace-cabinet" src="wp-content/themes/entrecieletmer/assets/img/trace_cabinet.png">
+        <div class="container-content-cabinet">
+            <div class="container-txt-cabinet">
+                <h2 class="title-cabinet">le cabinet chaleureux</h2>
+                <p>Je vous reçois, sur rendez-vous, à mon cabinet situé à Sainte-Savine, dans un cadre cosy et sécurisant.
+                    Naître&Bien’être est un lieu qui allie le soin au bien-être.
+                    Un espace chaleureux, douillet et vivant qui met en avant l’Humai</p>
+                <button>prendre rendez vous</button>
+            </div>
+            <div class="container-img-cabinet">
+                <img class="img-cabinet" src="wp-content/themes/entrecieletmer/assets/img/cabinet_massage.jpg">
+            </div>
+        </div>
+    </section>
+    <section class="container-prestations">
+        <div class="bg-prestations"></div>
+        <h2>mes prestations</h2>
+        <div class="container-grid-prestations">
+            <div class="card-prestation">
+                <div class="content-card">
+                    <img class="img-prestation" src="wp-content/themes/entrecieletmer/assets/img/la_femme.png">
+                    <h3>titre prestation</h3>
+                    <p>description</p>
+                    <button>découvrir</button>
+                </div>
+            </div>
+            <div class="card-prestation">
+                <div class="content-card">
+                    <img class="img-prestation" src="wp-content/themes/entrecieletmer/assets/img/la_future_maman.png">
+                    <h3>titre prestation</h3>
+                    <p>description</p>
+                    <button>découvrir</button>
+                </div>
+            </div>
+            <div class="card-prestation">
+                <div class="content-card">
+                    <img class="img-prestation" src="wp-content/themes/entrecieletmer/assets/img/la_mere.png">
+                    <h3>titre prestation</h3>
+                    <p>description</p>
+                    <button>découvrir</button>
+                </div>
+            </div>
+            <div class="card-prestation">
+                <div class="content-card">
+                    <img class="img-prestation" src="wp-content/themes/entrecieletmer/assets/img/les_bebes.png">
+                    <h3>titre prestation</h3>
+                    <p>description</p>
+                    <button>découvrir</button>
+                </div>
+            </div>
+            <div class="card-prestation">
+                <div class="content-card">
+                    <img class="img-prestation" src="wp-content/themes/entrecieletmer/assets/img/les_forfaits.png">
+                    <h3>titre prestation</h3>
+                    <p>description</p>
+                    <button>découvrir</button>
+                </div>
+            </div>
+            <div class="card-prestation">
+                <div class="content-card">
+                    <img class="img-prestation" src="wp-content/themes/entrecieletmer/assets/img/les_ateliers_de_groupe.png">
+                    <h3>titre prestation</h3>
+                    <p>description</p>
+                    <button>découvrir</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 <?php get_footer();?>
